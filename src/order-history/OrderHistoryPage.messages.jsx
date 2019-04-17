edd22115f279d6821dd9f0df1dbb9764e2483102
Adam Butterworth <adamjbutterworth@gmail.com>
2019-04-17 11:56:12 -0400

    feat: update api service to transform data and render order history table (#7)
    * feat: update api service to transform data. render order history table
    
    * fix: i18n the strings

--- src/order-history/OrderHistoryPage.messages.jsx
+++ src/order-history/OrderHistoryPage.messages.jsx
@@ -21,6 +21,31 @@ const messages = defineMessages({
     defaultMessage: 'Error: {error}',
     description: 'Message when orders are fail to load',
   },
+  'ecommerce.order.history.view.order.detail': {
+    id: 'ecommerce.order.history.view.order.detail',
+    defaultMessage: 'View Order Details',
+    description: 'Link to an order receipt',
+  },
+  'ecommerce.order.history.table.column.items': {
+    id: 'ecommerce.order.history.table.column.items',
+    defaultMessage: 'Items',
+    description: 'The column label for Items in the order history table.',
+  },
+  'ecommerce.order.history.table.column.date.placed': {
+    id: 'ecommerce.order.history.table.column.date.placed',
+    defaultMessage: 'Date placed',
+    description: 'The column label for Date placed in the order history table.',
+  },
+  'ecommerce.order.history.table.column.total.cost': {
+    id: 'ecommerce.order.history.table.column.total.cost',
+    defaultMessage: 'Total cost',
+    description: 'The column label for Total cost in the order history table.',
+  },
+  'ecommerce.order.history.table.column.order.number': {
+    id: 'ecommerce.order.history.table.column.order.number',
+    defaultMessage: 'Order number',
+    description: 'The column label for Order number in the order history table.',
+  },
 });
 
 export default messages;
--- src/order-history/service.js
+++ src/order-history/service.js
@@ -31,31 +31,94 @@ export async function getOrders() {
     setTimeout(() => {
       resolve([
         {
-          id: 2,
-          order_date: 'date',
-          total: 41.65,
-          currency: 'USD',
-          detailsUrl: 'http://edx.org',
-          description: 'Seat in Introduction to Animal Behaviour with verified certificate (and ID verification)',
-        },
-        {
-          id: 3,
-          order_date: 'date',
-          total: 41.65,
-          currency: 'USD',
-          detailsUrl: 'http://edx.org',
-          description: 'Seat in Introduction to Animal Behaviour with verified certificate (and ID verification)',
-        },
-        {
-          id: 4,
-          order_date: 'date',
-          total: 41.65,
-          currency: 'USD',
-          detailsUrl: 'http://edx.org',
-          description: 'Seat in Introduction to Animal Behaviour with verified certificate (and ID verification)',
+          order_date: 'Jan 24, 2019',
+          price: '41.65',
+          lines: [
+            {
+              status: 'Complete',
+              unit_price_excl_tax: '49.00',
+              product: {
+                attribute_values: [
+                  {
+                    code: 'certificate_type',
+                    name: 'certificate_type',
+                    value: 'verified',
+                  },
+                  {
+                    code: 'course_key',
+                    name: 'course_key',
+                    value: 'course-v1:WageningenX+AB101x+3T2018',
+                  },
+                  {
+                    code: 'id_verification_required',
+                    name: 'id_verification_required',
+                    value: true,
+                  },
+                ],
+                stockrecords: [
+                  {
+                    price_currency: 'USD',
+                    product: 27189,
+                    partner_sku: 'B48291A',
+                    partner: 1,
+                    price_excl_tax: '49.00',
+                    id: 19312,
+                  },
+                ],
+                product_class: 'Seat',
+                title: 'Seat in Introduction to Animal Behaviour with verified certificate (and ID verification)',
+                url: 'https://ecommerce.edx.org/api/v2/products/27189/',
+                price: '49.00',
+                expires: '2019-08-29T23:00:00Z',
+                is_available_to_buy: true,
+                id: 27189,
+                structure: 'child',
+              },
+              line_price_excl_tax: '41.65',
+              description: 'Seat in Introduction to Animal Behaviour with verified certificate (and ID verification)',
+              title: 'Seat in Introduction to Animal Behaviour with verified certificate (and ID verification)',
+              quantity: 1,
+            },
+          ],
+          number: 'EDX-34827343',
+          receipt_url: 'https://ecommerce.edx.org/checkout/receipt/?order_number=EDX-34827343',
         },
       ]);
     }, 500);
   });
-  return data;
+
+  const transformedOrdersData = data.map(({
+    order_date, // eslint-disable-line camelcase
+    price,
+    lines,
+    number,
+    receipt_url, // eslint-disable-line camelcase
+  }) => {
+    const lineItems = lines.map(({
+      product,
+      status,
+      description,
+      title,
+      expires,
+      quantity,
+    }) => ({
+      itemId: product.id,
+      status,
+      description,
+      title,
+      expires,
+      quantity,
+    }));
+
+    return {
+      orderDate: order_date,
+      total: price,
+      orderId: number,
+      receiptUrl: receipt_url,
+      currency: lines[0].product.stockrecords[0].price_currency, // This is bad!
+      lineItems,
+    };
+  });
+
+  return transformedOrdersData;
 }
