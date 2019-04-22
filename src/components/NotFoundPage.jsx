56c2312be4726014948d08cab06d1c2a96d4e60a
David Joy <davidjoy@users.noreply.github.com>
2019-04-22 16:45:40 -0400

    Ignore the temp directory, fix messages, update transifex_input.json (#6)
    * Ignore the temp directory, update transifex_input.json
    
    * Updating error pages to not be profile-specific.

--- src/components/NotFoundPage.jsx
+++ src/components/NotFoundPage.jsx
@@ -6,7 +6,7 @@ export default function NotFoundPage() {
     <div className="container-fluid d-flex py-5 justify-content-center align-items-start text-center">
       <p className="my-0 py-5 text-muted" style={{ maxWidth: '32em' }}>
         <FormattedMessage
-          id="profile.notfound.message"
+          id="error.notfound.message"
           defaultMessage="The page you're looking for is unavailable or there's an error in the URL. Please check the URL and try again."
           description="error message when a page does not exist"
         />
