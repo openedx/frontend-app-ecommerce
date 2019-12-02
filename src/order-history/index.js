d0142ab4916f394b844296a59468f0049db39f4d
Adam Butterworth <abutterworth@edx.org>
2019-12-02 13:29:45 -0500

    feat: use frontend-platform (#74)

--- src/order-history/index.js
+++ src/order-history/index.js
@@ -1,13 +1,11 @@
 import ConnectedOrderHistoryPage from './OrderHistoryPage';
 import reducer from './reducer';
 import saga from './saga';
-import { configureApiService } from './service';
 import { storeName } from './selectors';
 
 export {
   ConnectedOrderHistoryPage,
   reducer,
   saga,
-  configureApiService,
   storeName,
 };
