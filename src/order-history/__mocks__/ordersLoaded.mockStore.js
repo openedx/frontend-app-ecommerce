439277247cb622c40d9e31a8067403395b0dcdbd
David Joy <davidjoy@users.noreply.github.com>
2021-06-08 11:02:22 -0400

    fix: Miscellaneous updates to prepare for an Open edX release (#153)
    * docs: updating the README to conform to our template
    
    - Adding environment variable details
    - Tweaking installation/running instructions to match template
    - Adding an image of the MFE’s UI
    
    * build: updating site name in dev/test to be “localhost”
    
    This matches other MFEs.
    
    * build: removing unused environment variables
    
    All these environment variables don’t exist anymore and need to be cleaned up.
    
    * build: fixing environment variable default values
    
    An environment variable with a default of `null` becomes a default of `”null”`, which is truthy instead of falsy.  Not really what we want.  They were all null originally because we didn’t fully understand how the dotenv package worked.
    
    Also adding a protocol to the ORDER_HISTORY_URL, which was badly formatted.
    
    * test: cleaning up bad test data in mock store
    
    All this data was left over from a prior version of this MFE (or perhaps copied from profile during development) - none of it is used at all and doesn’t exist in the actual redux store.
    
    * fix: using siteName instead of hard-coded edX
    
    The “no orders” message had a hard-coded edX in it.  This fixes that and uses SITE_NAME instead.
    
    * docs: adding a note about npm ci usage

--- src/order-history/__mocks__/ordersLoaded.mockStore.js
+++ src/order-history/__mocks__/ordersLoaded.mockStore.js
@@ -1,87 +1,4 @@
 module.exports = {
-  authentication: {
-    userId: 9,
-    username: 'staff'
-  },
-  configuration: {
-    BASE_URL: 'localhost:1996',
-    LMS_BASE_URL: 'http://localhost:18000',
-    ECOMMERCE_BASE_URL: 'http://localhost:18130',
-    CREDENTIALS_BASE_URL: 'http://localhost:18150',
-    LOGIN_URL: 'http://localhost:18000/login',
-    LOGOUT_URL: 'http://localhost:18000/login',
-    CSRF_TOKEN_API_PATH: '/csrf/api/v1/token',
-    REFRESH_ACCESS_TOKEN_ENDPOINT: 'http://localhost:18000/login_refresh',
-    SEGMENT_KEY: null,
-    ACCESS_TOKEN_COOKIE_NAME: 'edx-jwt-cookie-header-payload',
-    USER_INFO_COOKIE_NAME: 'edx-user-info',
-    LANGUAGE_PREFERENCE_COOKIE_NAME: 'openedx-language-preference',
-    SITE_NAME: 'edX',
-    MARKETING_SITE_BASE_URL: 'http://localhost:18000',
-    ENTERPRISE_MARKETING_URL: 'http://example.com',
-    ENTERPRISE_MARKETING_UTM_CAMPAIGN: 'test_campaign',
-    ENTERPRISE_MARKETING_UTM_SOURCE: 'orders',
-    ENTERPRISE_MARKETING_FOOTER_UTM_MEDIUM: 'Footer',
-    SUPPORT_URL: 'http://localhost:18000/support',
-    CONTACT_URL: 'http://localhost:18000/contact',
-    OPEN_SOURCE_URL: 'http://localhost:18000/openedx',
-    TERMS_OF_SERVICE_URL: 'http://localhost:18000/terms-of-service',
-    PRIVACY_POLICY_URL: 'http://localhost:18000/privacy-policy',
-    FACEBOOK_URL: 'https://www.facebook.com',
-    TWITTER_URL: 'https://twitter.com',
-    YOU_TUBE_URL: 'https://www.youtube.com',
-    LINKED_IN_URL: 'https://www.linkedin.com',
-    REDDIT_URL: 'https://www.reddit.com',
-    APPLE_APP_STORE_URL: 'https://www.apple.com/ios/app-store/',
-    GOOGLE_PLAY_URL: 'https://play.google.com/store',
-    ACCOUNT_SETTINGS_URL: 'http://localhost:18000/account/settings',
-    SECURE_COOKIES: false,
-    ENVIRONMENT: 'development',
-    ACCOUNTS_API_BASE_URL: 'http://localhost:18000/api/user/v1/accounts',
-    PREFERENCES_API_BASE_URL: 'http://localhost:18000/api/user/v1/preferences',
-    CERTIFICATES_API_BASE_URL: 'http://localhost:18000/api/certificates/v0/certificates',
-    VIEW_MY_RECORDS_URL: 'http://localhost:18150/records',
-    ECOMMERCE_API_BASE_URL: 'http://localhost:18130/api/v2',
-    ECOMMERCE_RECEIPT_BASE_URL: 'http://localhost:18130/checkout/receipt/'
-  },
-  userAccount: {
-    loading: false,
-    loaded: true,
-    error: null,
-    username: 'staff',
-    email: 'staff@example.com',
-    bio: 'skasldjaksjdlkksadasdasasd',
-    name: 'Lemon Seltzer',
-    country: 'AM',
-    socialLinks: [
-      {
-        platform: 'facebook',
-        socialLink: 'https://www.facebook.com/ljhl'
-      }
-    ],
-    profileImage: {
-      imageUrlFull: 'http://localhost:18000/static/images/profiles/default_500.png',
-      imageUrlLarge: 'http://localhost:18000/static/images/profiles/default_120.png',
-      imageUrlMedium: 'http://localhost:18000/static/images/profiles/default_50.png',
-      imageUrlSmall: 'http://localhost:18000/static/images/profiles/default_30.png',
-      hasImage: false
-    },
-    levelOfEducation: null,
-    mailingAddress: null,
-    extendedProfile: [],
-    dateJoined: '2017-06-07T00:44:23Z',
-    accomplishmentsShared: false,
-    isActive: true,
-    yearOfBirth: 1918,
-    goals: null,
-    languageProficiencies: [],
-    courseCertificates: null,
-    requiresParentalConsent: false,
-    secondaryEmail: null,
-    timeZone: null,
-    gender: null,
-    accountPrivacy: 'custom'
-  },
   orderHistory: {
     loading: false,
     loadingError: null,
@@ -134,13 +51,5 @@ module.exports = {
     currentPage: 2,
     next: null,
     previous: null
-  },
-  router: {
-    location: {
-      pathname: '/orders',
-      search: '',
-      hash: ''
-    },
-    action: 'POP'
   }
 };
