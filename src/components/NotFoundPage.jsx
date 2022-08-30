1c144187e0793adea059e551e7420397a259062b
julianajlk <julianajlk@gmail.com>
2022-08-30 10:52:16 -0400

    fix: Modify NotFoundPage error span conditional

--- src/components/NotFoundPage.jsx
+++ src/components/NotFoundPage.jsx
@@ -2,8 +2,7 @@ import React from 'react';
 import { FormattedMessage } from '@edx/frontend-platform/i18n';
 import PropTypes from 'prop-types';
 
-export default function NotFoundPage(props) {
-  const { error } = props;
+export default function NotFoundPage({ error }) {
   return (
     <div className="container-fluid d-flex py-5 justify-content-center align-items-start text-center">
       <p className="my-0 py-5 text-muted" style={{ maxWidth: '32em' }}>
@@ -12,7 +11,7 @@ export default function NotFoundPage(props) {
           defaultMessage="The page you're looking for is unavailable or there's an error in the URL. Please check the URL and try again."
           description="error message when a page does not exist"
         />
-        <span>{error}</span>
+        { error ? <span>{error}</span> : null }
       </p>
     </div>
   );
