7f7820553a5eed0d9bd30e62b89f93fa1c4e2fb9
Nawfal Ahmed <nawfal.ahmed@arbisoft.com>
2023-05-24 17:41:19 +0500

    refactor: remove unused AsyncActionType, favour saga routines over this

--- src/utils/utils.test.js
+++ src/utils/utils.test.js
@@ -1,5 +1,4 @@
 import {
-  AsyncActionType,
   modifyObjectKeys,
   camelCaseObject,
   snakeCaseObject,
@@ -107,18 +106,6 @@ describe('keepKeys', () => {
       one: 123,
       six: null,
       8: 'sneaky',
-    });
-  });
-
-  describe('AsyncActionType', () => {
-    it('should return well formatted action strings', () => {
-      const actionType = new AsyncActionType('HOUSE_CATS', 'START_THE_RACE');
-
-      expect(actionType.BASE).toBe('HOUSE_CATS__START_THE_RACE');
-      expect(actionType.BEGIN).toBe('HOUSE_CATS__START_THE_RACE__BEGIN');
-      expect(actionType.SUCCESS).toBe('HOUSE_CATS__START_THE_RACE__SUCCESS');
-      expect(actionType.FAILURE).toBe('HOUSE_CATS__START_THE_RACE__FAILURE');
-      expect(actionType.RESET).toBe('HOUSE_CATS__START_THE_RACE__RESET');
     });
   });
 });
