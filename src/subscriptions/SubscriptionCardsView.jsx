23fcf914f03625fefb3152a8c8b113602155b822
Nawfal Ahmed <111358247+NawfalAhmed@users.noreply.github.com>
2023-06-16 15:30:23 +0500

    feat: add translations for subscription state badges (#305)

--- src/subscriptions/SubscriptionCardsView.jsx
+++ src/subscriptions/SubscriptionCardsView.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { getConfig } from '@edx/frontend-platform';
-
+import { useIntl } from '@edx/frontend-platform/i18n';
 import { Badge, Card, Hyperlink } from '@edx/paragon';
+
+import messages from './SubscriptionCardsView.messages';
 
 const STATUS_VARIANT_MAP = {
   active: 'success',
@@ -10,34 +12,49 @@ const STATUS_VARIANT_MAP = {
   inactive: 'light',
 };
 
-const SubscriptionCardsView = ({ subscriptions }) => (
-  <div className="section section-gap-lg">
-    {subscriptions.map(({
-      uuid,
-      title,
-      organizations,
-      status,
-    }) => (
-      <Hyperlink
-        key={uuid + status}
-        destination={`${getConfig().LMS_BASE_URL}/dashboard/programs/${uuid}`}
-      >
+const STATUS_MESSAGE_ID_MAP = {
+  active: 'ecommerce.order.history.subscriptions.badge.active',
+  trial: 'ecommerce.order.history.subscriptions.badge.trial',
+  inactive: 'ecommerce.order.history.subscriptions.badge.inactive',
+};
+
+const SubscriptionCardsView = ({ subscriptions }) => {
+  const { formatMessage } = useIntl();
+
+  const renderItem = ({
+    uuid,
+    title,
+    organizations,
+    status,
+  }) => {
+    const url = getConfig().LMS_BASE_URL;
+
+    const key = uuid + status;
+    const destination = `${url}/dashboard/programs/${uuid}`;
+    const variant = STATUS_VARIANT_MAP[status];
+    const message = STATUS_MESSAGE_ID_MAP[status];
+
+    return (
+      <Hyperlink key={key} destination={destination}>
         <Card className="bg-light-200 p-3">
           <div className="section flex-column-reverse flex-sm-row align-items-start align-items-sm-center">
             <h3 className="text-info-500 m-0">{title}</h3>
-            <Badge
-              className="text-capitalize"
-              variant={STATUS_VARIANT_MAP[status]}
-            >
-              {status}
+            <Badge className="text-capitalize" variant={variant}>
+              {message ? formatMessage(messages[message]) : status}
             </Badge>
           </div>
           <p className="small text-gray-500 m-0">{organizations.join(', ')}</p>
         </Card>
       </Hyperlink>
-    ))}
-  </div>
-);
+    );
+  };
+
+  return (
+    <div className="section section-gap-lg">
+      {subscriptions.map(renderItem)}
+    </div>
+  );
+};
 
 SubscriptionCardsView.propTypes = {
   subscriptions: PropTypes.arrayOf(
