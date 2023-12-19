2a35e6953c8416674b8efffaf6b690662a5e6b5b
Juliana Kang <julianajlk@gmail.com>
2023-12-19 10:16:17 -0500

    refactor: Modify initial loading state for better control of subs section (#356)
    REV-3799

--- src/orders-and-subscriptions/OrdersAndSubscriptionsPage.messages.js
+++ src/orders-and-subscriptions/OrdersAndSubscriptionsPage.messages.js
@@ -3,8 +3,8 @@ import { defineMessages } from '@edx/frontend-platform/i18n';
 const messages = defineMessages({
   'ecommerce.order.history.loading': {
     id: 'ecommerce.order.history.loading',
-    defaultMessage: 'Loading orders and subscriptions...',
-    description: 'Message when orders and subscriptions page is loading.',
+    defaultMessage: 'Loading orders...',
+    description: 'Message when order history page is loading.',
   },
 });
 
